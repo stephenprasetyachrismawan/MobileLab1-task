5e48b0d342a8898aec3e62dfa80d8a3818a90219
stephenprasetyachrismawan <80116750+stephenprasetyachrismawan@users.noreply.github.com>
2023-09-05 16:52:23 +0000

    ready to submit

--- main.dart
+++ main.dart
@@ -6,10 +6,13 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = (DateTime.now().year - tahunLahir);
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021025", "Stephen Prasetya Chrismawan", 2004);
+  mhs.perkenalan();
 }
